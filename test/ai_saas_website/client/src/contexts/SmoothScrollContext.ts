56129a421fe8b7755ca75796707f5d15e33d21be
csevenx <ouubahim1@gmail.com>
2025-04-08 00:07:01 -0400

    final

--- test/ai_saas_website/client/src/contexts/SmoothScrollContext.ts
+++
@@ -1,8 +0,0 @@
-import { createContext, RefObject } from 'react';
-import Lenis from 'lenis';
-
-export interface SmoothScrollContextType {
-  lenis: RefObject<Lenis | null>;
-}
-
-export const SmoothScrollContext = createContext<SmoothScrollContextType | null>(null);
